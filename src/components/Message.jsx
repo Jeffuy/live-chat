7958694e2a27d6885b3d760ce35fe027ecd5cd82
Jeff Free <jefffreelance3@gmail.com>
2022-09-07 00:19:08 -0300

    messages added

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
-  return (
-	<div>Message</div>
-  )
-}
+	return (
+		<div className="message owner">
+			<div className="messageInfo">
+				<img
+					src="https://images.pexels.com/photos/13195312/pexels-photo-13195312.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+					alt="avatar"
+				/>
+				<span>Just Now</span>
+			</div>
+			<div className="messageContent">
+				<p>Hello</p>
+				<img
+					src="https://images.pexels.com/photos/13195312/pexels-photo-13195312.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default Message
+export default Message;
