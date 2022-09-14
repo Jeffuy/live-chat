1026495e9bbe4210aa0b981aa182156679da6b43
Jeff Free <jefffreelance3@gmail.com>
2022-09-14 14:02:24 -0300

    fixed img error

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -36,9 +36,9 @@ const Chats = () => {
 		<div className="chats">
 			{Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
 				<div className="userChat" key={chat[0]} onClick={()=>handleSelect(chat[1].userInfo)}>
-					<img src={chat[1].userInfo.photoURL} alt="Avatar" />
+					<img src={chat[1].userInfo?.photoURL} alt="Avatar" />
 					<div className="userChatInfo">
-						<span>{chat[1].userInfo.displayName}</span>
+						<span>{chat[1].userInfo?.displayName}</span>
 						<p>{chat[1].lastMessage?.text}</p>
 					</div>
 				</div>
